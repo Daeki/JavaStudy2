383d4d5e3f08db960504f8a964bc8029763ae7c3
merandie <kimdaeki@gmail.com>
2025-01-23 12:13:43 +0900

    User1

--- src/com/winter/app/ztest/TestMain.java
+++ src/com/winter/app/ztest/TestMain.java
@@ -5,8 +5,13 @@ public class TestMain {
 	public static void main(String[] args) {
 		try {
 			
-			DBConnectionTest test = new DBConnectionTest();
-			test.getConnectionTest();
+//			DBConnectionTest test = new DBConnectionTest();
+//			test.getConnectionTest();
+			
+			UserDAOTest userDAOTest = new UserDAOTest();
+			
+			//userDAOTest.joinTest();
+			userDAOTest.getListTest();
 			
 			
 			
